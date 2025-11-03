94e858f43bbf01e7cc135444666185215e8193bb
Gabriel Messias <gabrielmessias@outlook.com>
2025-11-03 14:11:38 -0300

    add a lista de categorias no home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,5 @@
+import ListaCategorias from "../../components/categoria/listacategorias/ListaCategorias"
+
 function Home() {
   return (
     <>
@@ -22,6 +24,7 @@ function Home() {
           </div>
         </div>
       </div>
+      <ListaCategorias />
     </>
   )
 }
